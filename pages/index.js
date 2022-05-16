2a3d97958c7f1829ec4f6c889e72724136bacf5b
Vina Eka Laylani <vina.eka@justdelegate.co>
2022-05-17 05:33:07 +0700

    private route

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,74 @@
-import PageComp from '../components/page'
-import SideBar from '../components/sidebar'
-import { Row, Col, Container } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
-import searchApi from '../services/api';
+import { Row, Col, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+import PageComp from "../components/page";
+import SideBar from "../components/sidebar";
+import searchApi from "../services/api";
+import { error } from "../components/swal";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const [leaves, setLeaves] = useState([])
-  const [user, setUser] = useState({})
-  const [initial, setInitial] = useState("")
+  const [leaves, setLeaves] = useState([]);
+  const [user, setUser] = useState({});
+  const [initial, setInitial] = useState("");
+  const [auth, setAuth] = useState(false);
+  const router = useRouter();
 
   const getInitial = async () => {
     try {
-      const res = await searchApi.getInitial()
-      setInitial(res)
-    } catch (error) {
-      console.log(error)
+      const res = await searchApi.getInitial();
+      setInitial(res);
+    } catch (err) {
+      error(err);
     }
-  }
+  };
 
   const getUserLogin = async () => {
     try {
-      const res = await searchApi.getUserLogin()
-      setUser(res)
-    } catch (error) {
-      console.log(error)
+      const res = await searchApi.getUserLogin();
+      setLeaves(
+        res?.Leaves.sort((a, b) => (a.updatedAt > b.updatedAt ? -1 : 1))
+      );
+      setUser(res);
+    } catch (err) {
+      error(err);
     }
-  }
+  };
 
-  const getLeaves = async () => {
-    try {
-      const res = await searchApi.LeaveUserLogin()
-      setLeaves(res)
-    } catch (error) {
-      console.log(error)
+  const Auth = () => {
+    const access_token = localStorage.getItem("access_token");
+    if (!access_token) {
+      router.push("/login");
+    } else {
+      setAuth(true);
     }
-  }
+  };
 
   useEffect(() => {
-    getUserLogin()
-    getInitial()
-    getLeaves()
-  }, [])
+    Auth();
+    if (auth) {
+      getUserLogin();
+      getInitial();
+    }
+  }, []);
 
   return (
     <Container className="d-xl-block d-none">
-      <Row className="container">
-        <Col xs={3}>
-          <SideBar />
-        </Col>
-        <Col xs={9}>
-          <PageComp user={user} leaves={leaves} initial_user={initial} setLeaves={setLeaves} />
-        </Col>
-      </Row>
+      {auth && (
+        <Row className="container">
+          <Col xs={3}>
+            <SideBar />
+          </Col>
+          <Col xs={9}>
+            <PageComp
+              user={user}
+              leaves={leaves}
+              initial={initial}
+              setLeaves={setLeaves}
+            />
+          </Col>
+        </Row>
+      )}
     </Container>
-  )
+  );
 }
