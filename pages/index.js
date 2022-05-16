c928658e96af715f91291f74850e48ef36a8fe10
Vina Eka Laylani <vina.eka@justdelegate.co>
2022-05-17 05:29:59 +0700

    update api

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,14 @@ import PageComp from "../components/page";
 import SideBar from "../components/sidebar";
 import searchApi from "../services/api";
 import { error } from "../components/swal";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const [leaves, setLeaves] = useState([])
+  const [leaves, setLeaves] = useState([]);
   const [user, setUser] = useState({});
   const [initial, setInitial] = useState("");
+  const [auth, setAuth] = useState(false);
+  const router = useRouter();
 
   const getInitial = async () => {
     try {
@@ -23,28 +26,49 @@ export default function Home() {
   const getUserLogin = async () => {
     try {
       const res = await searchApi.getUserLogin();
-      setLeaves(res?.Leaves.sort((a, b) => (a.updatedAt > b.updatedAt ? -1 : 1)))
+      setLeaves(
+        res?.Leaves.sort((a, b) => (a.updatedAt > b.updatedAt ? -1 : 1))
+      );
       setUser(res);
     } catch (err) {
       error(err);
     }
   };
 
+  const Auth = () => {
+    const access_token = localStorage.getItem("access_token");
+    if (!access_token) {
+      router.push("/login");
+    } else {
+      setAuth(true);
+    }
+  };
+
   useEffect(() => {
-    getUserLogin();
-    getInitial();
+    Auth();
+    if (auth) {
+      getUserLogin();
+      getInitial();
+    }
   }, []);
 
   return (
     <Container className="d-xl-block d-none">
-      <Row className="container">
-        <Col xs={3}>
-          <SideBar />
-        </Col>
-        <Col xs={9}>
-          <PageComp user={user} leaves={leaves} initial={initial} setLeaves={setLeaves} />
-        </Col>
-      </Row>
+      {auth && (
+        <Row className="container">
+          <Col xs={3}>
+            <SideBar />
+          </Col>
+          <Col xs={9}>
+            <PageComp
+              user={user}
+              leaves={leaves}
+              initial={initial}
+              setLeaves={setLeaves}
+            />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 }
